3210051fbbf4ebde1cbf3287b6400e13ad36d15d
Prabal Deshar <prabaldeshar@gmail.com>
2024-11-24 20:49:28 +0545

    add: chroma db in gitignore

--- document_chatbot/settings.py
+++ document_chatbot/settings.py
@@ -136,3 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
